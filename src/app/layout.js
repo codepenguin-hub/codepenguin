1acd8e150a5ef909f60cf938163ec29ff34e2357
BrayanASS <proffleandro.linux@gmail.com>
2024-05-29 22:40:49 -0300

    fixed gsap bug in page.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,20 +1,20 @@
-import { Barlow } from "next/font/google";
-import "./globals.css";
+import { Poppins } from "next/font/google";
+import "../styles/globals.css";
 
-const barlow = Barlow({
-	subsets: ["latin"],
-	weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
 });
 
 export const metadata = {
-	title: "Code Penguin",
-	description: "Desenvolvimento WEB é com a gente!",
+  title: "Code Penguin",
+  description: "Desenvolvimento WEB é com a gente!",
 };
 
 export default function RootLayout({ children }) {
-	return (
-		<html lang="en">
-			<body className={barlow.className}>{children}</body>
-		</html>
-	);
+  return (
+    <html lang="en">
+      <body className={poppins.className}>{children}</body>
+    </html>
+  );
 }
