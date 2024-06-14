97055526d56674e4e95839e2e7100f74eafc0dd5
andrewmvk <andrew.medeiros00@gmail.com>
2024-06-14 12:26:43 -0300

    Now it should work

--- src/components/CustomImage.js
+++ src/components/CustomImage.js
@@ -1,9 +1,9 @@
 import Image from "next/image";
 import React from "react";
 
-const isProd = process.env.NODE_ENV === "production";
+const isDev = process.env.NODE_ENV === "development";
 
-const basePath = isProd ? "/codepenguin" + "." : "";
+const basePath = isDev ? "" : ".";
 
 export default React.forwardRef((props, ref) => {
 	return <Image src={basePath + props.src} {...props} ref={ref} />;
